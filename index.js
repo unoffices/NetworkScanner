f6d8fe67a5d158b184d87a0139834735deb10e01
Iain Brown <codeiain@outlook.com>
2017-01-19 08:55:19 +0000

    updated to add a notify prop on devices

--- index.js
+++ index.js
@@ -16,54 +16,67 @@ for (var x = 1; x != 255; x++) {
 var devices = [
     {
         'name': 'Router',
-        'ip': '192.168.1.1'
+        'ip': '192.168.1.1',
+        'notify': false
     }, {
         'name': 'File Server',
-        'ip': '192.168.1.2'
+        'ip': '192.168.1.2',
+        'notify': true
     },
     {
         'name': 'Ethernet via power',
-        'ip': '192.168.1.3'
+        'ip': '192.168.1.3',
+        'notify': false
     },
     {
         'name': 'Ethernet via power',
-        'ip': '192.168.1.4'
+        'ip': '192.168.1.4',
+        'notify': false
     },
     {
         'name': 'Media Server',
-        'ip': '192.168.1.5'
+        'ip': '192.168.1.5',
+        'notify': true
     },
     {
         'name': 'Youview',
-        'ip': '192.168.1.6'
+        'ip': '192.168.1.6',
+        'notify': true
     },
     {
         'name': 'Ethernet via power',
-        'ip': '192.168.1.7'
+        'ip': '192.168.1.7',
+        'notify': false
     },
     {
         'name': 'Chromecast',
-        'ip': '192.168.1.8'
+        'ip': '192.168.1.8',
+        'notify': true
     },
     {
         'name': 'Ethernet via power',
-        'ip': '192.168.1.10'
+        'ip': '192.168.1.10',
+        'notify': fasle
     },
     {
         'name': 'Bed Room Tv',
-        'ip': '192.168.1.11'
+        'ip': '192.168.1.11',
+        'notify': true
     },
     {
         'name': 'Dev Server',
-        'ip': '192.168.1.18'
+        'ip': '192.168.1.18',
+        'notify': true
     },
     {
         'name': "Jenni's iPhone",
-        'ip': '192.168.1.15'
+        'ip': '192.168.1.15',
+        'notify': false
     },
     {
-        'name':'Iain`s iPhone',
-        'ip':'192.168.1.9'
+        'name': 'Iain`s iPhone',
+        'ip': '192.168.1.9',
+        'notify': false
     }
 ]
 
@@ -75,56 +88,54 @@ function GetByIp(arr, value) {
 
 }
 
-new CronJob('0 */30 * * * *', function() {
+new CronJob('0 */30 * * * *', function () {
 
 
-var count = 0;
-hosts.forEach(function (host) {
+    var count = 0;
+    hosts.forEach(function (host) {
 
-    var _self = this;
-    _self.msg = '';
-    ping.sys.probe(host, function (isAlive) {
-        redisClient.get(host, function (err, reply) {
-            var msg = ''
-            var name = GetByIp(devices, host);
+        var _self = this;
+        _self.msg = '';
+        ping.sys.probe(host, function (isAlive) {
+            redisClient.get(host, function (err, reply) {
+                var msg = ''
+                var name = GetByIp(devices, host);
 
-            if (name != undefined) {
-                msg = isAlive ? name.name + " " + host + ' is alive' : name.name + " " + host + ' is dead';
-            } else {
-                msg = isAlive ? "unkown " + host + " is alive" : '';
-            }
-            if (reply != isAlive.toString()) {
-                if (msg != '') {
-                    console.log(err);
-                    slack.alert(msg, function (err) {
-                        if (err) {
-                            console.log(err);
-                        }
-                    });
+                if (name != undefined) {
+                    msg = isAlive ? name.name + " " + host + ' is alive' : name.name + " " + host + ' is dead';
+                } else {
+                    msg = isAlive ? "unkown " + host + " is alive" : '';
                 }
-            }
-            redisClient.set(host, isAlive);
-            count++;
-            if (count == hosts.length) {
-                var currentdate = new Date();
-                var datetime = "Last Sync: " + currentdate.getDate() + "/"
-                    + (currentdate.getMonth() + 1) + "/"
-                    + currentdate.getFullYear() + " @ "
-                    + currentdate.getHours() + ":"
-                    + currentdate.getMinutes() + ":"
-                    + currentdate.getSeconds() + "\n";
-                fs.appendFile('logs.txt', datetime, function (err) {
-                   
-                });
+                if (name.notify){
+                    if (reply != isAlive.toString()) {
+                        if (msg != '') {
+                            console.log(err);
+                            slack.alert(msg, function (err) {
+                                if (err) {
+                                    console.log(err);
+                                }
+                            });
+                        }
+                    }
+                }
+                redisClient.set(host, isAlive);
+                count++;
+                if (count == hosts.length) {
+                    var currentdate = new Date();
+                    var datetime = "Last Sync: " + currentdate.getDate() + "/"
+                        + (currentdate.getMonth() + 1) + "/"
+                        + currentdate.getFullYear() + " @ "
+                        + currentdate.getHours() + ":"
+                        + currentdate.getMinutes() + ":"
+                        + currentdate.getSeconds() + "\n";
+                    fs.appendFile('logs.txt', datetime, function (err) {
 
-            }
-        })
+                    });
+
+                }
+            })
+
+        });
 
     });
-
-});
-
-
-
-
 }, null, true, 'America/Los_Angeles');
